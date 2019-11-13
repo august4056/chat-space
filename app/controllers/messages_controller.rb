1a243680bef77fdf7b8288672705a3be94eafcb1
Takahiro Kojima <57321070+august4056@users.noreply.github.com>
2019-11-13 15:11:03 +0900

    Revert "Chat-Spaceの非同期通信によるメッセージ送信"

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,23 +9,20 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-    respond_to do |format|
-      format.html { redirect_to group_messages_path, notice: "messageをそうしんした" }
-      format.json
-    end
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されたよ'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してね'
       render :index
     end
   end
-  
+
   private
-  
+
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
-  
+
   def set_group
     @group = Group.find(params[:group_id])
   end
