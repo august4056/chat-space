6d8eb6b96be812d43f773c28c7d0ede051ca4f32
august4056 <57321070+august4056@users.noreply.github.com>
2019-11-15 20:52:02 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'coffeemilk4056'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/coffeemilk4056'
 end
